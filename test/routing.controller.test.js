76474d4ac237d99846a33fddb602c2136a916bd5
capan <capanh@gmail.com>
2020-02-08 23:40:34 +0300

    Create routing.controller.test.js

---
+++ test/routing.controller.test.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+
+let app; let request;
+describe('routing', () => {
+    beforeEach(() => {
+        app = require('../server');
+        request = supertest(app);
+    });
+    // afterEach(() => {
+        // app.close();
+    // });
+    it('should return 200 status', async (done) => {
+        const res = await request.get('/api/getrouteposition/39.937/32.824/39.934/32.823');
+        expect(res.status).toBe(200);
+        done();
+    });
+    it('should return a valid json for given coordinates', async (done) => {
+        const res = await request.get('/api/getrouteposition/39.937/32.824/39.934/32.823');
+        let isValid;
+        try {
+            JSON.parse(res.body.data[0].st_asgeojson);
+            isValid = true;
+        } catch (err) {
+            isValid = false;
+        }
+        expect(isValid).toBe(true);
+        done();
+    });
+});
